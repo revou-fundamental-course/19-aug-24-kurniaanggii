99accd8f606aef66d2ca5ea4cee71e04d611b6fb
Anggi Dwi Kurniawan <kurniaanggii@gmail.com>
2024-08-30 01:56:13 +0700

    update

--- js/script.js
+++ js/script.js
@@ -61,3 +61,28 @@ function showBanner(indexBanner) {
 setInterval(function () {
   nextSlide(1);
 }, 3000);
+
+
+//menampilkan submit form
+function setSenderUI(nama, tanggalLahir, jenisKelamin, pesan) {
+    document.getElementById("sender-nama").innerHTML = nama;
+    document.getElementById("sender-tanggal-lahir").innerHTML = tanggalLahir;
+    document.getElementById("sender-jenis-kelamin").innerHTML = jenisKelamin;
+    document.getElementById("sender-pesan").innerHTML = pesan;
+  }
+  
+  //memvalidasi form jika kosong
+  function validateform() {
+    const nama = document.forms["message-form"]["nama"].value;
+    const tanggalLahir = document.forms["message-form"]["tanggal-lahir"].value;
+    const jenisKelamin = document.forms["message-form"]["jenis-kelamin"].value;
+    const pesan = document.forms["message-form"]["pesan"].value;
+  
+    if (nama == "" || tanggalLahir == "" || jenisKelamin == "" || pesan == "") {
+      alert("Tidak boleh ada yang kosong");
+      return false;
+    }
+  
+    setSenderUI(nama, tanggalLahir, jenisKelamin, pesan);
+    return false;
+  }
